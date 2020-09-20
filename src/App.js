694460329c9926614dacb225496ebf3febb74056
thang <whymydear99@gmail.com>
2020-09-21 05:42:38 +0700

    add weather report

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 //COMPONENTS
 import Header from "./components/Header"
+import Weather from "./components/Weather"
 
 //STYLE
 import './App.css';
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <Weather />
     </div>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,16 +3,23 @@ import React from 'react';
 const Weather = () => {
     return(
         <section>
-            <h1>London</h1>
-            <table>
-                <td>
-                    <ul>
-                        <li>Monday</li>
-                        <li>Min: 10 &deg; C</li>
-                        <li>Max: 30 &deg; C</li>
-                    </ul>
-                </td>
-            </table>
+            <h1
+            className="location">London</h1>
+            <div
+            className="weather-container">
+                <div
+                className="weather-box">
+                    <p>Today</p>
+                    <p>Min: 4.35 &deg;C</p>
+                    <p>Max: 13.05 &deg;C</p>
+                </div>
+                <div
+                className="weather-box">
+                    <p>Tomorrow</p>
+                    <p>Min: 4.35 &deg;C</p>
+                    <p>Max: 13.05 &deg;C</p>
+                </div>
+            </div>
         </section>
     )
 }
