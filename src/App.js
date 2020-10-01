5e35d3ef90b1f40d631e9129a6e473bfb61f8089
thang <whymydear99@gmail.com>
2020-10-01 10:12:29 +0700

    create weather report

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Axios from 'axios';
 
 //COMPONENTS
 import Weather from "./components/Weather"
@@ -7,16 +7,29 @@ import LocationInput from "./components/LocationInput"
 
 //STYLE
 import './App.css';
-import Axios from 'axios';
+
 
 
 class App extends React.Component {
   state = {
       title: undefined,
-      id: undefined,
-      minTemp: undefined,
-      maxTemp: undefined,
+      woeid: undefined,
       suggestions: undefined,
+      day1: undefined,
+      day1mintemp: undefined,
+      day1maxtemp: undefined,
+      day2: undefined,
+      day2mintemp: undefined,
+      day2maxtemp: undefined,
+      day3: undefined,
+      day3mintemp: undefined,
+      day3maxtemp: undefined,
+      day4: undefined,
+      day4mintemp: undefined,
+      day4maxtemp: undefined,
+      day5: undefined,
+      day5mintemp: undefined,
+      day5maxtemp: undefined,
   }
   searchLocation = (text) => {
     if (text) {
@@ -46,27 +59,42 @@ class App extends React.Component {
     .then(res => {
       this.setState({
         title: res.data[0].title,
-        id: res.data[0].woeid,
+        woeid: res.data[0].woeid,
         suggestions: undefined
       })
-      this.getWeatherByID(this.state.id)
-    })
-      
-  }
-  getWeatherByID = (woeid) => {
-    const url = `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`
-    Axios.get(url, {
-      headers: {
-        'x-requested-with': null
-      }
-    })
-    .then(res => {
-      this.setState({
-        maxTemp: res.data.consolidated_weather[0].max_temp,
-        minTemp: res.data.consolidated_weather[0].min_temp,
-      })
-    })
-  }
+      this.getWeatherReports(this.state.woeid)
+    })}
+  getWeatherReports = (id) => {
+      if (id !== undefined) {
+        const url = `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${id}/`
+        Axios.get(url, {
+          headers: {
+            'x-requested-with': null
+          }
+        })
+        .then(res => {
+          return(
+            this.setState({
+              day1: res.data.consolidated_weather[0].applicable_date,
+              day1mintemp: res.data.consolidated_weather[0].min_temp,
+              day1maxtemp: res.data.consolidated_weather[0].max_temp,
+              day2: res.data.consolidated_weather[1].applicable_date,
+              day2mintemp: res.data.consolidated_weather[1].min_temp,
+              day2maxtemp: res.data.consolidated_weather[1].max_temp,
+              day3: res.data.consolidated_weather[2].applicable_date,
+              day3mintemp: res.data.consolidated_weather[2].min_temp,
+              day3maxtemp: res.data.consolidated_weather[2].max_temp,
+              day4: res.data.consolidated_weather[3].applicable_date,
+              day4mintemp: res.data.consolidated_weather[3].min_temp,
+              day4maxtemp: res.data.consolidated_weather[3].max_temp,
+              day5: res.data.consolidated_weather[4].applicable_date,
+              day5mintemp: res.data.consolidated_weather[4].min_temp,
+              day5maxtemp: res.data.consolidated_weather[4].max_temp,
+            })
+          )
+        })
+        }
+    }
   render() {
     
   return (
@@ -77,9 +105,21 @@ class App extends React.Component {
       getLocationID={this.getLocationID}
       suggestions={this.state.suggestions} />
       <Weather
-      title={this.state.title}
-      minTemp={this.state.minTemp}
-      maxTemp={this.state.maxTemp}
+      day1={this.state.day1}
+      day1mintemp={this.state.day1mintemp}
+      day1maxtemp={this.state.day1maxtemp}
+      day2={this.state.day2}
+      day2mintemp={this.state.day2mintemp}
+      day2maxtemp={this.state.day2maxtemp}
+      day3={this.state.day3}
+      day3mintemp={this.state.day3mintemp}
+      day3maxtemp={this.state.day3maxtemp}
+      day4={this.state.day4}
+      day4mintemp={this.state.day4mintemp}
+      day4maxtemp={this.state.day4maxtemp}
+      day5={this.state.day5}
+      day5mintemp={this.state.day5mintemp}
+      day5maxtemp={this.state.day5maxtemp}
       />
     </div>
   );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,36 @@
 import React from 'react';
-
 const Weather = (props) => {
-    const { title , maxTemp , minTemp } = props
+    const { day1, day1mintemp, day1maxtemp, day2, day2mintemp, day2maxtemp, day3, day3mintemp, day3maxtemp, day4, day4mintemp, day4maxtemp, day5, day5mintemp, day5maxtemp } = props
+    
     return(
     <section>
-        {title && <div>
-          <h1>{title}</h1>
-        <div className="weather-report">
-            <div>
-              {maxTemp && <p>{maxTemp} &deg;C</p>}
-              {minTemp && <p>{minTemp} &deg;C</p>}
-            </div>
-        </div>
-          </div>}
+      {day1 && <ul className="weather-report">
+        <li className="day-weather-report">
+          <p>{day1}</p>
+          <p>Min: {day1mintemp}&deg;C</p>
+          <p>Max: {day1maxtemp}&deg;C</p>
+        </li>
+        <li className="day-weather-report">
+          <p>{day2}</p>
+          <p>Min: {day2mintemp}&deg;C</p>
+          <p>Max: {day2maxtemp}&deg;C</p>
+        </li>
+        <li className="day-weather-report">
+          <p>{day3}</p>
+          <p>Min: {day3mintemp}&deg;C</p>
+          <p>Max: {day3maxtemp}&deg;C</p>
+        </li>
+        <li className="day-weather-report">
+          <p>{day4}</p>
+          <p>Min: {day4mintemp}&deg;C</p>
+          <p>Max: {day4maxtemp}&deg;C</p>
+        </li>
+        <li className="day-weather-report">
+          <p>{day5}</p>
+          <p>Min: {day5mintemp}&deg;C</p>
+          <p>Max: {day5maxtemp}&deg;C</p>
+        </li>
+      </ul>}
     </section>
     )
 }
