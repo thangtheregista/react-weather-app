ac0a52a6de7cb52ec940eecb24fc9d970c31a86d
= <=>
2020-10-01 11:57:54 +0700

    As a user, I would like to see the app heading and input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
+//COMPONENTS
+import Header from './components/Header'
+
+//STYLES
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
