d3d55c692670db7a2d1b788fef070f3a1d78feb5
thang <whymydear99@gmail.com>
2020-10-01 11:02:58 +0700

    add app header and input

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header  = () => {
+    return(
+        <header>
+            <p className="app-heading">Weather Forecast App</p>
+        <div
+        className="location-input-box">
+            <input 
+            placeholder="Search"
+            />
+        </div>
+        </header>
+    )
+}
+
+export default Header
