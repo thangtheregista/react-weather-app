0279664b94d3699dbc2bfc21f85c26887a22a8f4
= <=>
2020-10-02 08:56:38 +0700

    As an user, I can search for locations by typing in the searching input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
 import React from 'react';
+import Axios from 'axios';
+import { debounce } from 'debounce';
 
 //COMPONENTS
-import Header from './components/Header'
+import Header from './components/Header';
 
 //STYLES
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    suggestions: undefined,
+    loading: false,
+    error: false,
+  }
+
+  setLoadingToTrue = () => {
+    this.setState({
+      loading: true
+    })
+  }
+
+  searchLocation = debounce((text) => {
+    this.setLoadingToTrue()
+    if (text.length > 2) {
+      const url = `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${text}`
+      Axios.get(url, {
+        headers: {
+          'x-requested-with': null
+        }
+      })
+      .then(res => {
+        const cityNames = res.data.slice(0, 5).map(city => city.title)
+
+        if (cityNames.length === 0) {
+          this.setState({
+            suggestions: undefined,
+            loading: false,
+            error: true,
+          })
+        } else {
+          this.setState({
+            suggestions: cityNames,
+            loading: false,
+            error: false,
+          })
+        }
+    })
+    } else {
+      this.setState({
+        suggestions: undefined,
+        loading: false,
+        error: false,
+      })
+    }
+  }, 500)
+
+  render () {
+    return (
+      <div className="App">
+        <Header 
+          searchLocation={this.searchLocation}
+          suggestions={this.state.suggestions}
+          loading={this.state.loading}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,49 @@
-import React from 'react';
-
-const Header  = () => {
+import React, { useState } from 'react';
+const Header = (props) => {
+  const { searchLocation, suggestions, loading, error } = props;
+  const [city, setCity] = useState("");
   return(
     <header>
       <p className="app-heading">Weather Forecast App</p>
-      <div
-      className="location-input-box">
+      <div className="location-input-box">
         <div className="mag-glass-div">
-          <img src="/magnifying-glass.svg" alt="magnifying-glass" className="mag-glass"
-          />
+          {loading ? (<img src="/Spinner-1s-200px.svg" alt="Loading" className="spinner" />) : (<img src="/magnifying-glass.svg" alt="magnifying-glass" className="mag-glass"/>)}
         </div>
-        <input 
-        placeholder="Search"           
+        <input
+          onChange={(e) => {
+            setCity(e.target.value)
+            searchLocation(e.target.value)
+          }}
+          type="text"
+          value={city} 
+          placeholder="Search"
         />
       </div>
+      { !suggestions && error === false && 
+        <div className="input-status">
+          Please search for a city.
+        </div> 
+      }
+      { error === true && 
+        <ul className="suggestions-box">
+          <li className="suggestion">
+            No results.
+          </li>
+        </ul>
+      }
+      { suggestions && 
+        <ul className="suggestions-box">
+          { suggestions.map((suggestion) => (
+            <li
+              className="suggestion"
+              key={suggestion}>
+                {suggestion}
+            </li>
+          ))}
+        </ul>
+      }
     </header>
-    )
+  )
 }
 
 export default Header
